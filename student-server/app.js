7d0670a387ee538d859136638f09b9cf6817e741
SoniyaDas31 <dassuubulakshmi@gmail.com>
2024-12-16 22:42:03 +0530

    Login API test backend

--- student-server/app.js
+++ student-server/app.js
@@ -1,13 +1,14 @@
-const express=require('express');
-const mongoose=require('mongoose');
+const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
-const projectRouter=require('./routers/projectRouter');
-const studentRouter=require('./routers/studentenrollRouter');
-const projectModel=require('./models/projectModel');
-const studentModel=require('./models/studentModel');
-const upload=require('./multer');
-const cors= require('cors');
-const app=express();
+const projectRouter = require('./routers/projectRouter');
+const studentRouter = require('./routers/studentenrollRouter');
+const loginRouter = require('./routers/loginRouter');
+const projectModel = require('./models/projectModel');
+const studentModel = require('./models/studentModel');
+const upload = require('./multer');
+const cors = require('cors');
+const app = express();
 
 
 
@@ -15,17 +16,17 @@ const app=express();
 // app.use(express.json())
 app.use(cors());
 app.use(express.json());
-app.use('/project',projectRouter);
-app.use('/students',studentRouter);
+app.use('/project', projectRouter);
+app.use('/students', studentRouter);
 app.use('/uploads', express.static('uploads'));
-mongoose.connect(process.env.mongodb_url).then(()=>{
+app.use('/login', loginRouter);
+mongoose.connect(process.env.mongodb_url).then(() => {
     console.log("connection established");
-    })
-    .catch((err)=>{ 
+})
+    .catch((err) => {
         console.log(err);
     });
-    app.listen(process.env.port,()=>{
-        console.log(`server running in port ${process.env.port}`);
-    });
-    
-    
+app.listen(process.env.port, () => {
+    console.log(`server running in port ${process.env.port}`);
+});
+
